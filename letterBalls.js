385ee0aa3d3fd1a08cbfda4bdab6989ad67c00da
llam16 <lydia.lam104@gmail.com>
2021-08-08 15:38:01 -0700

    Make home section including letter balls responsive

--- letterBalls.js
+++ letterBalls.js
@@ -1,11 +1,34 @@
-/* global dist, keyCode, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, collideRectCircle, collideCircleCircle, random, mouseIsPressed, clear, textSize, createCanvas, strokeWeight, rect, background, colorMode, HSB, noStroke, backgroundColor, color, fill, ellipse, text, stroke, line, width, height, mouseX, mouseY */
+/* global 
+  collideCircleCircle, 
+  random, 
+  textSize, 
+  createCanvas, 
+  strokeWeight, 
+  background, 
+  colorMode, 
+  HSB, 
+  noStroke, 
+  color, 
+  fill,
+  circle,
+  text, 
+  stroke, 
+  width, 
+  height,
+  windowWidth,
+  windowHeight
+*/
 
-let dots, randomDots;
+let dots
 
 function setup() {
-  let canvas = createCanvas(605, 405);
+  let canvas = createCanvas(windowWidth*0.42, windowHeight*0.575);
   canvas.parent('letter-balls');
   colorMode(HSB, 360, 100, 100);
+  createDots();
+}
+
+function createDots() {
   dots = [];
   dots.push(new BouncyDot("L"));
   dots.push(new BouncyDot("Y"));
@@ -13,10 +36,16 @@ function setup() {
   dots.push(new BouncyDot(" I"));
   dots.push(new BouncyDot("A"));
 
-  randomDots = [];
   for (let i = 0; i < 10; i++) {
-      randomDots[i] = new RandomDot();
+      dots.push(new RandomDot());
   }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth*0.42, windowHeight*0.575);
+  dots.forEach(dot => {
+    dot.resizeRadius();
+  });
 }
 
 function draw() {
@@ -25,18 +54,10 @@ function draw() {
 }
 
 function drawDots() {
-  for (let i = 0; i < dots.length; i++) {
-      dots[i].float();
-      dots[i].display();
-  }
-  for (let i = 0; i < randomDots.length; i++) {
-      randomDots[i].float();
-      randomDots[i].display();
-  }
-}
-
-function mousePressed() {
-// We'll use this for console log statements only.
+  dots.forEach(dot => {
+    dot.float();
+    dot.display();
+  });
 }
 
 class RandomDot {
@@ -45,72 +66,24 @@ class RandomDot {
       this.x = random(width);
       this.y = random(height);
       // Randomly generate radius
-      this.r = random(15,30);
+      this.radiusPercent = random(0.008, 0.025);
+      this.r = windowWidth*this.radiusPercent;
       // Randomly generate color
       this.color = color(0,0,100);
       // Randomly generate a master velocity (broken into components)...
       this.minVelocity
-      this.masterXvelocity = random(0.5, 2);
-      this.masterYvelocity = random(0.5, 2);
+      this.masterXvelocity = random(windowWidth*0.0005, windowWidth*0.001);
+      this.masterYvelocity = random(windowWidth*0.0005, windowWidth*0.001);
       // ...and use those as starting velocities.
       this.xVelocity = this.masterXvelocity;
       this.yVelocity = this.masterYvelocity;
-      
+  }
+
+  resizeRadius() {
+    this.r = windowWidth*this.radiusPercent;
   }
 
   float() {
-
-      this.x += this.xVelocity;
-      this.y += this.yVelocity;
-      
-      
-      
-      // Standard bounce code - like the DVD logo, but for spheres.
-      if (this.x + this.r > width) {
-      this.xVelocity = -1 * this.masterXvelocity;
-      }
-      if (this.x - this.r < 0) {
-      this.xVelocity = this.masterXvelocity;
-      }
-      if (this.y + this.r > height) {
-      this.yVelocity = -1 * this.masterYvelocity;
-      }
-      if (this.y - this.r < 0) {
-      this.yVelocity = this.masterYvelocity;
-      }
-  }
-
-  display() {
-
-      fill(this.color, 50, 100);
-      noStroke();
-      ellipse(this.x, this.y, this.r * 2);
-  }
-}
-
-class BouncyDot {
-  constructor(letter) {
-      // Randomly generate position
-      this.x = random(width);
-      this.y = random(height);
-      // Randomly generate radius
-      this.r = 40;
-      // White color
-      this.color = color(0,0,100);
-      // Randomly generate a master velocity (broken into components)...
-      this.minVelocity
-      this.masterXvelocity = random(1, 2);
-      this.masterYvelocity = random(1, 2);
-      // ...and use those as starting velocities.
-      this.xVelocity = this.masterXvelocity;
-      this.yVelocity = this.masterYvelocity;
-      
-      this.letter = letter;
-  }
-
-
-  float() {
-
       this.x += this.xVelocity;
       this.y += this.yVelocity;
       
@@ -130,13 +103,62 @@ class BouncyDot {
   }
 
   display() {
-
       fill(this.color, 50, 100);
       noStroke();
-      ellipse(this.x, this.y, this.r * 2);
+      circle(this.x, this.y, this.r);
+  }
+}
+
+class BouncyDot {
+  constructor(letter) {
+      // Randomly generate position
+      this.x = random(width);
+      this.y = random(height);
+      // Randomly generate radius
+      this.r = windowWidth*0.03;
+      // White color
+      this.color = color(0,0,100);
+      // Randomly generate a master velocity (broken into components)...
+      this.minVelocity
+      this.masterXvelocity = random(windowWidth*0.001, windowWidth*0.0018);
+      this.masterYvelocity = random(windowWidth*0.001, windowWidth*0.0018);
+      // ...and use those as starting velocities.
+      this.xVelocity = this.masterXvelocity;
+      this.yVelocity = this.masterYvelocity;
+      
+      this.letter = letter;
+  }
+
+  resizeRadius() {
+    this.r = windowWidth*0.03;
+  }
+
+  float() {
+      this.x += this.xVelocity;
+      this.y += this.yVelocity;
+      
+      // Standard bounce code
+      if (this.x + this.r > width) {
+      this.xVelocity = -1 * this.masterXvelocity;
+      }
+      if (this.x - this.r < 0) {
+      this.xVelocity = this.masterXvelocity;
+      }
+      if (this.y + this.r > height) {
+      this.yVelocity = -1 * this.masterYvelocity;
+      }
+      if (this.y - this.r < 0) {
+      this.yVelocity = this.masterYvelocity;
+      }
+  }
+
+  display() {
+      fill(this.color, 50, 100);
+      noStroke();
+      circle(this.x, this.y, this.r);
       fill(0,0,0);
-      textSize(40);
-      strokeWeight(10);
+      textSize(windowWidth*0.03);
+      strokeWeight(windowWidth*0.01);
       text(this.letter, this.x-12,this.y+12);
   }
 }
